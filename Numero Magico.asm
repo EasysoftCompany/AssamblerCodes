c452331b83b4bdb3f737af036fb75eea3294d800
PC10 <PC10>
2016-03-03 16:26:28 +0100

    Numero Ganador

---
+++ Numero Magico.asm
@@ -0,0 +1,76 @@
+;*************************************************************************************************   
+;                                                                                                *
+;                                     INSTITUTO POLITECNICO NACIONAL                             *
+;                                                                                                *
+;                                                                                                *
+;                       UNIDAD PROFESIONAL INTERDISCIPLINARIA DE INGENIERIA                      *
+;                                                                                                *
+;                                Y CIENCIAS SOCIALES Y ADMISTRATIVAS                             *
+;                                                                                                *
+;                                                                                                *
+;                                                                                                *
+;                                                                                                *
+;                                          "NUMEROS GANADOR"                                     *
+;                                                                                                *
+;                                                                                                *
+;                                                                                                *
+;                                           PROGRAMA NO. 13                                      *
+;                                                                                                *
+;                                                                                                *
+;                                                                                                *
+;   DESCRIPCION: EL PROGRAMA SOLICITA AL USUARIO EL NUMERO PARA DETERMINAR SI ESTE ES UN NUMERO  *
+;                PRIMO O NO  Y MOSTRAR EN PANTALLA EL RESULTADO DE ESTA EVALUACION               *
+;                                                                                                *
+;                                                                                                *
+;                                                                                                *
+;                                                                                                *
+;                                                                                                *
+;                                                                                                *
+;*************************************************************************************************
+
+INCLUDE "EMU8086.INC"
+MOV DX, 33; #NUMERO GANADOR
+MOV BX, 0 ;CONTADOR
+     
+    CICLO:    
+    PRINTN "DAME UN NUMERO" 
+    CALL SCAN_NUM
+    
+    
+    CMP CX, 34 
+    JGE LEYENDA2
+    CMP CX, 32   
+    JBE LEYENDA
+    CMP CX, 33
+    JE LEYENDA3
+    
+    LEYENDA3:
+    MOV AX,BX
+     
+    PRINT " "
+    PRINT "FELICIDADES, LOGRASTE ADIVINAR EL NUMERO MAGICO EN "
+    CALL PRINT_NUM
+    PRINTN " INTENTOS!"
+    RET
+       
+    LEYENDA:
+    MOV AX,CX
+    ADD BX,1
+    PRINT " "
+    PRINT "EL NUMERO MAGICO ES MAYOR A: "
+    CALL PRINT_NUM
+    PRINTN " "
+    JMP CICLO
+    
+    LEYENDA2:
+    MOV AX,CX
+    ADD BX,1
+    PRINT " "
+    PRINT "EL NUMERO MAGICO ES MENOR A: "
+    CALL PRINT_NUM                       
+    PRINTN " "
+    JMP CICLO
+    
+    DEFINE_PRINT_NUM
+    DEFINE_SCAN_NUM   
+    DEFINE_PRINT_NUM_UNS
