368dbcc0584addd234bf0eed478ee6fc5a1e87ad
Gerardo Rico <g_rico_c@hotmail.com>
2016-03-16 21:03:02 -0600

    Lo de la clase del 15/03/16

---
+++ NUM-MAYOR.asm
@@ -0,0 +1,33 @@
+INCLUDE "EMU8086.INC"
+
+
+
+    CICLO:
+    PRINT "   DAME UN NUMERO:  "
+    CALL SCAN_NUM   
+    PRINTN " "
+    PRINT "  "
+    
+    
+    CMP CX, 999
+    JE EXIT 
+    
+    CMP CX,AX
+    JG MAYOR
+    
+    JMP CICLO
+    
+    
+    MAYOR:
+    MOV AX, CX
+    JMP CICLO
+    
+    
+    EXIT:
+    PRINT "EL NUMERO MAYOR ES: "
+    CALL PRINT_NUM
+    RET
+    
+    DEFINE_SCAN_NUM
+    DEFINE_PRINT_NUM
+    DEFINE_PRINT_NUM_UNS
